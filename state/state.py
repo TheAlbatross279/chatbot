5885c5ef85b84617a40becd535ce6eff81237e6b
Andrew Guenther <guenther.andrew.j@gmail.com>
2012-11-06 15:48:34 -0800

    No longer crashes if the user gives an unrecognized query to initiate conversation.

--- state/state.py
+++ state/state.py
@@ -63,7 +63,7 @@ class State:
       ((conf, context), state) = reduce(lambda x, y: x if x[0][0] > y[0][0] else y, confidence)
 
       if conf < 0.1:
-         if State.userState[nick] != State:
+         if not nick in State.userState or State.userState[nick] != State:
             State.userState[nick] = State
             return State.query(nick, msg)
          else:
