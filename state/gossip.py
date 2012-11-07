eecfc372998cad6feb6dd9f17f9f446400c51196
Kim Paterson <Cybermat@pcp067829pcs.wireless.calpoly.edu>
2012-11-07 10:53:37 -0800

    Fixed so knowers is now a set

--- state/gossip.py
+++ state/gossip.py
@@ -36,10 +36,10 @@ class Gossip(State):
                db.close_conn()
                responses = ["You already know everything I know!",
                             "I can not believe you enjoy gossip as" \
-                            "much as me, but I do not have any more gossip" \ 
-                            "to tell you now.",
-                            "I think you should go talk to others in this room,"\
-                                "because I am all out of gossip."]
+                            + "much as me, but I do not have any more gossip" \
+                            + "to tell you now.",
+                            "I think you should go talk to others in this room," \
+                            +   "because I am all out of gossip."]
 
                rand_ndx = random.randint(0, len(responses) - 1)
 
@@ -94,8 +94,18 @@ class Gossip(State):
                return "Hmmm... well, I don't really know anything right now...."
            else:
                #update the knowers to include the user asking for gossip
-               knowers = gossip[3] + "; " + context['_nick']
+               knowers = gossip[3].split("; ") 
+               knowers.append(context['_nick'])
                print knowers
+               
+               #add all users in the room to the knowers
+               if State.users != None:
+                   for user in State.users:
+                       knowers.append( user)
+
+               knowers = set(knowers)
+               print knowers
+               knowers = "; ".join(knowers)
                update_statement = "UPDATE facts SET knowers = \'" + knowers + \
                                   "\' WHERE author= \'" + gossip[0] + \
                                   "\' AND msg= \'" + gossip[1] + \
