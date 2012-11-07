d9cd77633c07996383d42b2c00f9601e964b9e2d
Jim Langlois <jlangloi698@gmail.com>
2012-11-06 17:43:04 -0800

    added the two states, but I have no registered them yet, because if I do it seems like they get cycled through, i have a Q or two when others are on

---
+++ state/userresponse.py
@@ -0,0 +1,30 @@
+from state import State
+from gossip import Gossip
+
+class UserResponse(State):
+    @staticmethod
+    def recognize(msg):
+      affirmative_words = ['yes', 'ya', 'sure', 'maybe', 'always']
+      negative_words = ['no', 'nope', 'never']
+      isAffirmative = False   
+   
+      for (w, tag) in msg:
+         if w.lower() in affirmative_words:
+            isAffirmative = True
+            return (1, {'isAffirmative': isAffirmative})
+
+      for (w, tag) in msg:
+         if w.lower() in negative_words:
+            isAffirmative = False
+            return (1, {'isAffirmative': isAffirmative})
+
+      return (0, {})
+
+    @staticmethod
+    def nextState(context):
+        return tuple([Gossip])
+        #if context['isAffirmative']:
+        #   return tuple([Gossip])
+        #return tuple([Gossip])
+
+#State.register(UserResponse)
