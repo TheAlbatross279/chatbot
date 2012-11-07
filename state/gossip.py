ecc2241b4e383283d2ab74ea242af19f49a837d9
kp <kp@Tardis.(none)>
2012-11-06 23:39:28 -0800

    Added affirmative state to gossip

--- state/gossip.py
+++ state/gossip.py
@@ -9,46 +9,47 @@ class Gossip(State):
 
         subject = None
 
-        if 'isAffirmative' not in context:
+        if context['isAffirmative]' == True:
            #if it's a targeted query, get the subject of the gossip
            if context['specific'] == True:
                subject = context['subject']
         
-        query = '''SELECT * FROM facts'''
-        db = Database()
-        results = db.query(query)
-        db.close_conn()
+           query = '''SELECT * FROM facts'''
+           db = Database()
+           results = db.query(query)
+           db.close_conn()
 
-        prefix = ["Did you know that ",
-                  "I heard that ", 
-                  "A little birdy told me that "]
+           prefix = ["Did you know that ",
+                     "I heard that ", 
+                     "A little birdy told me that "]
         
-        #select prefix
-        rand_ndx2 = random.randint(0, len(prefix)-1)        
+           #select prefix
+           rand_ndx2 = random.randint(0, len(prefix)-1)        
 
-        gossip = []
+           gossip = []
 
-        #if it's a speicifc query
-        if subject != None:
-            if subject in State.users:
-                return "Oh, " + subject + " is just so nice... nothing to say about them!"
+           #if it's a speicifc query
+           if subject != None:
+               if len(State.users) != 0 && subject in State.users:
+                   return "Oh, " + subject + " is just so nice... nothing to say about them!"
+               else:
+                   specific_results = [result for result in results if subject in result]
+                   rand_ndx = random.randint(0, len(specific_results)-1)            
+                   gossip = specific_results[rand_ndx]                
+           else: #randomly grab facts
+               rand_ndx = random.randint(0, len(results)-1)            
+               gossip = results[rand_ndx]            
+
+           response = prefix[rand_ndx2] + gossip[2] + " told " +  gossip[0] + ", \"" + gossip[1] + "\"!"
+
+            print gossip
+            print response
+
+            if len(gossip) == 0:
+                return "Hmmm... well, I don't really know anything right now...."
             else:
-               specific_results = [result for result in results if subject in result]
-               rand_ndx = random.randint(0, len(specific_results)-1)            
-               gossip = specific_results[rand_ndx]                
-        else: #randomly grab facts
-            rand_ndx = random.randint(0, len(results)-1)            
-            gossip = results[rand_ndx]            
-
-        response = prefix[rand_ndx2] + gossip[2] + " told " +  gossip[0] + ", \"" + gossip[1] + "\"!"
-
-        print gossip
-        print response
-
-        if len(gossip) == 0:
-            return "Hmmm... well, I don't really know anything right now...."
+                return response
         else:
-            return response
-        
+            return "Too bad... I had something really juicy!"
 
 State.register(Gossip, True)
