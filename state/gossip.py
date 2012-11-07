a8678e0cf100de814ce858fdf1aacc4bcddb82b0
kp <kp@Tardis.(none)>
2012-11-06 19:55:44 -0800

    Updated Gossip and RespondGossip to take into account speicifc queries about users

--- state/gossip.py
+++ state/gossip.py
@@ -6,6 +6,12 @@ import random
 class Gossip(State):
     @staticmethod
     def respond(context):
+
+        subject = None
+        #if it's a targeted query, get the subject of the gossip
+        if context['specific'] == True:
+            subject = context['subject']
+        
         query = '''SELECT * FROM facts'''
         db = Database()
         results = db.query(query)
@@ -13,13 +19,26 @@ class Gossip(State):
         prefix = ["Did you know that ",
                   "I heard that ", 
                   "A little birdy told me that "]
-        rand_ndx = random.randint(0, len(results)-1)
-        rand_ndx2 = random.randint(0, len(prefix)-1)
+        
+        #select prefix
+        rand_ndx2 = random.randint(0, len(prefix)-1)        
 
+        gossip = []
+        #if it's a speicifc query
+        if subject != None:
+            specific_results = [result for result in results if subject in result]
+            rand_ndx = random.randint(0, len(specific_results)-1)            
+            gossip = specific_results[rand_ndx]            
+            
+        else: #randomly grab facts
+            rand_ndx = random.randint(0, len(results)-1)            
+            gossip = results[rand_ndx]            
 
-        gossip = results[rand_ndx]
+        response = prefix[rand_ndx2] + gossip[2] + " told " +  gossip[0] + ", \"" + gossip[1] + "\"!"
+
         print gossip
-        response = prefix[rand_ndx2] + gossip[2] + " told " + gossip[0] + ", \"" + gossip[1] + "\"!"
+        print response
+
         return response
         
 
--- state/respondgossip.py
+++ state/respondgossip.py
@@ -4,13 +4,37 @@ from state import State
 class RespondGossip(Gossip):
     @staticmethod
     def recognize(msg):
-        tell_me_gossip = ["gossip"]
+        tell_me_gossip = ["gossip", "secret"]
+        keywords = [, "tell", "me", "do", "you", "know"]
+        count = 0
+        isGossip = False
 
-        for idx, w in enumerate(tell_me_gossip):
-            if msg[idx][0].lower() == w:
-                return (1.0, {})
+        #determine if specific query
+        isSpecific = False
+        subject = None
 
-        return (0.0, {})
+        #TODO NEED LIST OF USERS
+        users = []
+
+        for idx, w in enumerate(msg):
+            if w in keywords:
+                count+=1
+            elif w in tell_me_gossip:
+                isGossip = True
+            elif w in users:
+                isSpecific = True
+                subject = w
+
+        #confidence is high that it's gossip
+        if isGossip:
+            confidence = 1
+        #we're a little less confident it's gossip
+        elif count/len(keywords) >= .10:
+            confidence = count/len(keywords)
+            
+        
+        return (confidence, {'specific': isSpecific, 
+                             'subject': subject}
 
 
 State.register(RespondGossip, True)
