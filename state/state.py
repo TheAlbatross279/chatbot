69ea28180257f9eae1b5b17da929c0bcb24a841d
Andrew Guenther <guenther.andrew.j@gmail.com>
2012-11-07 02:01:03 -0800

    Reimplimented the confidence conditions

--- state/state.py
+++ state/state.py
@@ -65,12 +65,11 @@ class State:
       ((conf, context), state) = reduce(lambda x, y: x if x[0][0] > y[0][0] else y, confidence)
 
       if conf < 0.1:
-         #if not nick in State.userState or State.userState[nick] != State:
-         #   State.userState[nick] = State
-         #   return State.query(nick, msg)
-         #else:
-         #   return "I have no idea what's going on"
-         return None
+         if not nick in State.userState or State.userState[nick] != State:
+            State.userState[nick] = State
+            return State.query(nick, msg)
+         else:
+            return None
 
       State.userState[nick] = state
 
